97c523ca8c1f48db610feb9a12e8dfc47a1dfcdf
tavaresrafaela <tavares.rafaela2013@gmail.com>
2018-11-22 21:17:10 -0200

    Add arquivos pela segunda vez

--- app/src/main/java/com/example/tecinfo/finalaplication/MainActivity.java
+++ app/src/main/java/com/example/tecinfo/finalaplication/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.tecinfo.finalaplication;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        int id = menuItem.getItemId();
+
+        if (id == R.id.configuracao) {
+            Toast.makeText(getApplicationContext(),
+                    "Abrir configurações", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.calcularMedia) {
+            Toast.makeText(getApplicationContext(),
+                    "Abrir Calcular Média",Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.calculadora) {
+            Toast.makeText(getApplicationContext(),
+                    "Abrir Calculadora",Toast.LENGTH_SHORT).show();
+        }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
